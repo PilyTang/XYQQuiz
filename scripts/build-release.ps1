bc65844a3ce8ce7eb7d5e757c289e642e5080939
PilyTang <PilyTang@users.noreply.github.com>
2026-09-11 20:57:58 +0800

    Prepare 0.5.5 package and update project documentation

--- scripts/build-release.ps1
+++ scripts/build-release.ps1
@@ -1,6 +1,6 @@
 param(
     [string]$Python = ".\.venv\Scripts\python.exe",
-    [string]$Version = "0.5.4",
+    [string]$Version = "0.5.5",
     [string]$Commit = "",
     [string]$OutputDirectory = ".\release",
     [switch]$AllowDevelopmentCommit
